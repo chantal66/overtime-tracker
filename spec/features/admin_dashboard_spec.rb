ed9636a97e6881f4e4f412b2280fd6de126f1673
Chantal Justamond <cjustamm66@me.com>
2018-04-05 12:43:34 -0600

    test: admins can reach admin dashboard. :white_check_mark:

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -21,4 +21,13 @@ describe 'admin dashboard' do
 
     expect(current_path).to eq(root_path)
   end
+
+  it 'can be reached by admin users' do
+    admin_user = FactoryGirl.create(:admin_user)
+    login_as(admin_user, :scope => :user)
+
+    visit admin_root_path
+
+    expect(current_path).to eq(admin_root_path)
+  end
 end
